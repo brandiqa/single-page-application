06462ef339e72567a59443129b273251d65a3c98
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-13 19:20:41 +0300

    Implemented fixer api service

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { getRates } = require('./lib/fixer-service');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,13 @@ app.use(express.static('public'));
 
 // Provide access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+// Fetch Currency Rates
+app.get('/api/rates', async (req, res) => {
+  const data = await getRates();
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
+});
 
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
