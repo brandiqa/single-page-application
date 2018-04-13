edfa43a286524ae0ce4d70e471e36519b6e030fd
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-13 20:15:42 +0300

    Added active menu on select

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,12 @@
 window.addEventListener('load', () => {
   const el = $('#app');
   el.html('SPA Rocks!');
+
+  // Highlight Active Menu
+  $('a').on('click', (event) => {
+    event.preventDefault();
+    const target = $(event.target);
+    $('.item').removeClass('active');
+    target.addClass('active');
+  });
 });
